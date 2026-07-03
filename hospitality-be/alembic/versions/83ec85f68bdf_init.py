17d24461d639ea8d3492e9a7ae69dff8b53f2d21
nidhigokani2811 <you@example.com>
2026-07-03 11:03:39 +0530

    Please provide the list of files or the specific changes made so I can generate the commit message for you.

--- hospitality-be/alembic/versions/83ec85f68bdf_init.py
+++ hospitality-be/alembic/versions/83ec85f68bdf_init.py
@@ -1,18 +1,19 @@
-"""initial_squashed
+"""init
 
-Revision ID: 6e4f95b06d6a
+Revision ID: 83ec85f68bdf
 Revises: 
-Create Date: 2026-07-03 10:37:32.916516
+Create Date: 2026-07-03 10:52:19.876314
 
 """
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import sqlmodel
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6e4f95b06d6a'
+revision: str = '83ec85f68bdf'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -61,7 +62,7 @@ def upgrade() -> None:
     sa.Column('owner_id', sa.Integer(), nullable=True),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=False),
-    sa.ForeignKeyConstraint(['owner_id'], ['user.id'], ),
+
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_restaurant_name'), 'restaurant', ['name'], unique=False)
